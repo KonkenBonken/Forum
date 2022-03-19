4c9e56e705ef04b968ffa570350ded78a74bf080
KonkenBonken <konrad05pettersson@gmail.com>
2022-03-19 16:14:47 +0100

    Avoided duplicate users and added save agrument

--- script.ts
+++ script.ts
@@ -5,7 +5,12 @@ const Database = require('save-on-change')('database.json');
 
 const getNewId = require('randomnorepeat').createRandomString(10, Object.keys(Database.users));
 
+const findUserByUsername =
+  // @ts-expect-error
+  (username: string) => Object.values(Database.users).find((user) => user.username == username);
+
 class User {
+  success: boolean;
   id: string;
   username: string;
   age: number;
@@ -14,14 +19,17 @@ class User {
   static saveKeys = ['username', 'age', 'mail', 'avatarUrl']
 
 
-  constructor(options: { username: string, age: number, mail?: string, avatarUrl?: string }) {
-    this.id = getNewId();
+  constructor(options: { username: string, age: number, mail?: string, avatarUrl?: string }, id?: string, save: boolean = true) {
+    this.id = id ?? getNewId();
     this.username = options.username;
     this.age = options.age;
     this.mail = options.mail;
     this.avatarUrl = options.avatarUrl;
-    Database.users[this.id] = this;
-
+    this.success = !save;
+    if (save && !findUserByUsername(options.username)) {
+      this.success = true;
+      Database.users[this.id] = this;
+    }
   }
   toJSON(/*id:string*/) {
     return Object.fromEntries(
@@ -34,12 +42,6 @@ class User {
 }
 
 for (const id in Database.users)
-  Database.users[id] = new User(Database.users[id]);
+  Database.users[id] = new User(Database.users[id], id, false);
 
-
-new User({
-  username: 'KonkenBonken',
-  age: 17,
-  mail: 'konrad05pettersson@gmail.com'
-})
 console.log(Database);
